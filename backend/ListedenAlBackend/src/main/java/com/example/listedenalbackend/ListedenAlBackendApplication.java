adc9d358a79c1482e2b796e30352c67195d15e60
zahidayturan <21060607@stu.omu.edu.tr>
2025-07-22 21:59:39 +0300

    #init - initializes the backend project structure
    Sets up the basic project structure for the backend application.
    
    This includes:
    - Adding .gitignore and .gitattributes files for version control.
    - Including Maven Wrapper for consistent build environment.
    - Creating a basic Spring Boot application setup with necessary dependencies and configuration.

---
+++ backend/ListedenAlBackend/src/main/java/com/example/listedenalbackend/ListedenAlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.listedenalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListedenAlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListedenAlBackendApplication.class, args);
+    }
+
+}
---
+++ backend/ListedenAlBackend/src/test/java/com/example/listedenalbackend/ListedenAlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listedenalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListedenAlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
